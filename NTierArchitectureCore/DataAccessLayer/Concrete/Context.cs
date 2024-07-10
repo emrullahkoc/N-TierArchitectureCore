b2c905c9d42c63b552555212d424d82d42eb8b1c
emrullahkoc <emrullahkoc.1995@gmail.com>
2024-07-10 19:12:13 +0300

    Update

---
+++ NTierArchitectureCore/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+            "Server=EMRULLAH\\SQLEXPRESS;Database=TierArchitectureCore;" +
+            "TrustServerCertificate=True;Trusted_Connection=True;Encrypt=False"
+               );
+            }
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
--- NTierArchitectureCore/NTierArchitectureCore/obj/Debug/net8.0/NTierArchitectureCore.AssemblyInfo.cs
+++ NTierArchitectureCore/NTierArchitectureCore/obj/Debug/net8.0/NTierArchitectureCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NTierArchitectureCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c23398c4c692f7dedc2029b79425e6cc7984a5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e7924532f850ddb7321e4016e885cc63720b411")]
 [assembly: System.Reflection.AssemblyProductAttribute("NTierArchitectureCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NTierArchitectureCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
