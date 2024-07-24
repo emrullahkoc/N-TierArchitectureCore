aa607a4cad853bf1ed83ada7d53f7a73c7036e5f
emrullahkoc <emrullahkoc.1995@gmail.com>
2024-07-24 14:33:55 +0300

    admin teams

--- NTierArchitectureCore/NTierArchitectureCore/obj/Debug/net8.0/NTierArchitectureCore.AssemblyInfo.cs
+++ NTierArchitectureCore/NTierArchitectureCore/obj/Debug/net8.0/NTierArchitectureCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NTierArchitectureCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4269b1c3af0f6e9dc42d1a064ab33a2776e18dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06db7dfd9016f17fbd86437a91da6c5018804c3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NTierArchitectureCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NTierArchitectureCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
